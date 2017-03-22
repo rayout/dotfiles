b471a042c95672214107c02fe89edf5e7f972499
Mikhail Fesenko <proggga@gmail.com>
2017-03-22 18:52:03 +0100

    fix bug in args parser

--- bin/src/parse_args.sh
+++ bin/src/parse_args.sh
@@ -10,26 +10,27 @@ groups="";
 all_groups_not_checked="$(cat ./hosts.ini | grep -Eo '^\[.*\]$' | sed 's/\[\|\]//g')";
 all_groups=$(echo "$all_groups_not_checked" | while read line; do if [ -f "$line.yml" ] ; then echo $line.yml; fi; done)
 if [ -z "$first_arg" ] ; then
-    echo "Working by default for all hosts";
-    export groups=$all_groups;
-    echo $groups;
+    echo "Working by default with all hosts";
+    groups=$all_groups;
 else
     arg_groups=$(echo $first_arg| sed 's/,/\n/g')"";
     found_groups=$(echo "$arg_groups" | while read search_group; do
-        group=$(echo "$all_groups" | grep -E "^$search_group$" )
+        group=$(echo "$all_groups" | grep -E "^$search_group.yml$" )
         if [ "$group" ] ; then
             echo $group;
         fi;
     done)
     if [ -n "$found_groups" ] ; then
-        echo "working with "$(join $found_groups)" group";
+        groups=$found_groups;
+        found_groups=$(echo $found_groups|sed 's/\.yml//g')
+        echo "Working with "$(join , $found_groups)" group";
+        shift;
     else
-        export groups=$all_groups;
+        echo "Working by default with all hosts";
+        groups=$all_groups;
     fi;
-    shift;
 fi;
-if [[ $# -gt 1 ]]; then
+
+if [[ $# -gt 0 ]]; then
     tags="--tags=$(join , $@)"
 fi
-
-groups="$(join ' ' $groups)"
