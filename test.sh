f28b87a4cc5c8ff237d4f2f99d86ed9183b8e10b
Mikhail Fesenko <proggga@gmail.com>
2017-03-23 22:09:28 +0100

    now test can work in loop and tap modes

--- test.sh
+++ test.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-bats ./tests/*/*.test
+tap=''
+if [ "$1" == "--tap" ] || [ "$2" == "--tap" ] ; then
+	tap="--tap"
+fi;
+command="bats $tap ./tests/bash/*.test"
+$command
+while [ "$1" == "--loop" ] || [ "$2" == "--loop" ]; do
+	sleep 3;
+	$command;
+done;
